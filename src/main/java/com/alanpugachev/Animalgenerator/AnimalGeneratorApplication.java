f466c11fea8d302d3c72f860c6a6251586d881bc
Alan Pugachev <aszneesizzle@gmail.com>
2023-12-06 18:23:13 +0300

    Add reading function (should be replaced with simplier code)

--- src/main/java/com/alanpugachev/Animalgenerator/AnimalGeneratorApplication.java
+++ src/main/java/com/alanpugachev/Animalgenerator/AnimalGeneratorApplication.java
@@ -1,5 +1,6 @@
 package com.alanpugachev.Animalgenerator;
 
+import com.alanpugachev.Animalgenerator.models.Animal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class AnimalGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalGeneratorApplication.class, args);
+		Animal a = new Animal();
 	}
 
 }
--- src/main/java/com/alanpugachev/Animalgenerator/models/Animal.java
+++ src/main/java/com/alanpugachev/Animalgenerator/models/Animal.java
@@ -5,11 +5,54 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Random;
+
 @AllArgsConstructor
 @Setter
 @Getter
 public class Animal {
     private String kindOfAnimal;
     private String description;
+
+    public Animal() {
+        /* for kind */
+        try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(Paths.get("src/main/resources/Animals.txt"))) {
+            MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            StringBuilder kind = new StringBuilder();
+
+            /*
+            Random rand = new Random(0);
+            int position = rand.nextInt(byteBuffer.capacity());
+            */
+
+
+            for (int i = 0; i < byteBuffer.capacity(); i++) {
+                kind.append((char) byteBuffer.get());
+            }
+
+            this.kindOfAnimal = String.valueOf(kind);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read file with animals kind");
+        }
+
+        /* for description */
+        try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(Paths.get("src/main/resources/Descriptions.txt"))) {
+            MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            StringBuilder description = new StringBuilder();
+
+            for (int i = 0; i < byteBuffer.capacity(); i++) {
+                description.append((char) byteBuffer.get());
+            }
+
+            this.description = String.valueOf(description);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read file with animals description");
+        }
+    }
 }
